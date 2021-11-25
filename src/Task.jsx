b9ed0fc549d42b13d09f5ce13ec45cd8ee75bfa8
beamariano <64287361+beamariano@users.noreply.github.com>
2021-11-25 18:35:49 +0800

    to fix: drag

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { Draggable } from "react-beautiful-dnd";
 
 const Container = styled.div`
   border: 1px solid lightgrey;
   padding: 8px;
   margin-bottom: 8px;
   border-radius: 2px;
+  background-color: white;
 `;
 
 class Task extends Component {
   render() {
-    return <Container>{this.props.task.content}</Container>;
+    return (
+      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+        {(provided) => (
+          <Container
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            innerRef={provided.innerRef}
+          >
+            {this.props.task.content}
+          </Container>
+        )}
+      </Draggable>
+    );
   }
 }
 
