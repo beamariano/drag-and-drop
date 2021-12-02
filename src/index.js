71d510e51995c82748a39b53c2c79cc2c2167310
beamariano <64287361+beamariano@users.noreply.github.com>
2021-12-02 10:28:58 +0800

    feat: added: list reorder persists

--- src/index.js
+++ src/index.js
@@ -10,6 +10,33 @@ class App extends React.Component {
 
   onDragEnd = (result) => {
     //TODO:
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+    //draggableId is task ID
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: { ...this.state.columns, [newColumn.id]: newColumn },
+    };
+    this.setState(newState);
   };
 
   render() {
